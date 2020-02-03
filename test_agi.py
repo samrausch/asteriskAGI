bc7e044faa4e151c86181f314b0867d736387aa4
samrausch <samrausch@gmail.com>
2020-02-03 16:46:51 -0500

    continued work

--- test_agi.py
+++ test_agi.py
@@ -6,6 +6,7 @@
 
 from asterisk.agi import *
 import redis
+import sys
 
 r1 = redis.Redis(host='localhost', port=6379, db=0, password='fraudhappens')
 r2 = redis.Redis(host='localhost', port=6379, db=2, password='fraudhappens')
@@ -14,7 +15,14 @@ agi = AGI()
 
 agi.verbose("fraud_ivr agi started")
 
+contact = sys.argv[1]
+contactIP = contact.split("@")[1]
+contactIP = contactIP.split(":")[0]
+contactUser = contact.split("@")[0]
+contactUser = contactUser.split(" ")[1]
+contactUser = contactUser.split(":")[1]
 extension = agi.env['agi_callerid']
+
 scorequery = str(extension) + ":trustscore"
 pinquery = str(extension) + ":pin"
 score = int(r1.get(scorequery))
@@ -25,32 +33,32 @@ agi.verbose(agioutput1)
 agi.verbose(agioutput2)
 
 # agi.get_data timeout is in ms, not s as indicated in the documentation
-userinput = agi.get_data('vm-password', 2000, 4)
+userinput = agi.get_data('vm-password', 5000, 4)
 
 agi.verbose(userinput)
 if int(userinput) == pin:
-	newscore = int(score) - 10
-	if newscore < 0:
-		newscore = 0
-	agi.verbose(newscore)
-	r1.set(scorequery, newscore)
-	agi.stream_file('auth-thankyou')
-	agioutput3 = str(extension) + " PIN verified"
-	agi.verbose(agioutput3)
-	agi.hangup()
-	sys.exit()
+        newscore = int(score) - 10
+        if newscore < 0:
+                newscore = 0
+        agi.verbose(newscore)
+        r1.set(scorequery, newscore)
+        agi.stream_file('auth-thankyou')
+        agioutput3 = str(extension) + " PIN verified"
+        agi.verbose(agioutput3)
+        agi.hangup()
+        sys.exit()
 
 if int(userinput) != pin:
-	newscore = int(score) + 10
-	if newscore > 100:
-		newscore = 100
-	agi.verbose(newscore)
-	r1.set(scorequery, newscore)
-	agi.stream_file('vm-invalidpassword')
-	agioutput4 = str(extension) + " PIN verification failed!"
-	agi.verbose(agioutput4)
-	agi.hangup()
-	sys.exit()
+        newscore = int(score) + 10
+        if newscore > 100:
+                newscore = 100
+        agi.verbose(newscore)
+        r1.set(scorequery, newscore)
+        agi.stream_file('vm-invalidpassword')
+        agioutput4 = str(extension) + " PIN verification failed!"
+        agi.verbose(agioutput4)
+        agi.hangup()
+        sys.exit()
 
 agioutput5 = str(extension) + " an error occured in AGI verifying the PIN"
 agi.verbose(agioutput5)
